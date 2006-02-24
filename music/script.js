50f078e454684fa1fd0bfd9938feedcad0282090
unknown <akoppela@gmail.com>
2006-02-24 09:37:26 +0300

    Add animation sign-in

--- music/script.js
+++ music/script.js
@@ -1,3 +1,29 @@
-document.getElementById("login").click = function() {
-	document.getElementById("first-frame").style.display = "none";
-}
+window.addEvent('domready', function(){
+	
+	var myMorph1 = new Fx.Morph($('first-frame'));
+	var myMorph2 = new Fx.Morph($('second-frame'));
+	var mySlide = new Fx.Slide($('first-frame'), {transition: Fx.Transitions.Expo.easeInOut});
+
+	$('login').addEvent('click', function(e){
+		e.stop();
+		mySlide.slideOut();
+		myMorph1.start({
+			opacity : 0
+		});
+		myMorph2.start({
+			opacity : 1
+		});
+	});
+	
+	$('back').addEvent('click', function(e){
+		e.stop();
+		mySlide.slideIn();
+		myMorph1.start({
+			opacity : 1
+		});
+		myMorph2.start({
+			opacity : 0
+		});
+	});
+
+});
