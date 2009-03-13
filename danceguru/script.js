111fce88965b5f22ee3dea9524123ebd8622efab
unknown <akoppela@gmail.com>
2009-03-14 04:49:51 +0600

    fixed danceguru

--- danceguru/script.js
+++ danceguru/script.js
@@ -1,10 +1,11 @@
 window.addEvent('domready', function(){
 	
-	var bgcolor;
-	
+	var bgcolor, textcolor;
+		
 	$$('#header li div').addEvent('mouseenter', function(e){
 		bgcolor = $(this).getStyle('background-color');
 		this.style.background = "#fff";
+		textcolor = $(this).getLast().getFirst().getStyle('color');
 		$(this).getLast().getFirst().style.color = "#000";
 		$(this).getAllNext().each(function(name){
 			$(name).style.background = "#fff";
@@ -13,7 +14,7 @@ window.addEvent('domready', function(){
 	});
 	
 	$$('#header li div').addEvent('mouseleave', function(e){
-		$(this).getLast().getFirst().style.color = "#fff";
+		$(this).getLast().getFirst().style.color = textcolor;
 		this.style.background = bgcolor;
 		$(this).getAllNext().each(function(name){
 			$(name).style.background = bgcolor;
