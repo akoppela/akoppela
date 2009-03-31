668cf13962ecb20ca903114866dca35bc1ded0cd
unknown <akoppela@gmail.com>
2009-04-01 01:09:30 +0700

    fixed darts

--- sochi/script.js
+++ sochi/script.js
@@ -1,5 +1,17 @@
 window.addEvent('domready', function(){
 	
+	var slide1 = new Fx.Slide($('cat1'));
+	var slide2 = new Fx.Slide($('cat2'));
+	var slide3 = new Fx.Slide($('cat3'));
+	var slide4 = new Fx.Slide($('cat4'));
+	var slide5 = new Fx.Slide($('cat5'));
+	
+	$('cat1').slide('hide');
+	$('cat2').slide('hide');
+	$('cat3').slide('hide');
+	$('cat4').slide('hide');
+	$('cat5').slide('hide');
+	
 	var x = 0, rezheight1, heightcat1, height1;
 	
 	fheight = function (){
@@ -9,27 +21,100 @@ window.addEvent('domready', function(){
 		rezheight1 = heightcat1 - height1;	
 		return rezheight1;
 	}
+	
+	scrollbar = function(){
+		if(rezheight1 >= 0) {
+			$('cat-scrollbar').setStyles({visibility: 'visible'});
+		} else {
+			$('cat-scrollbar').setStyles({visibility: 'hidden'});
+		}
+	};
+	
+	catscroller = function () {
+		var scroll1 = new Slider($('cat-scroll-line'), $('cat-scroller'), { 
+			mode: 'vertical',
+			steps: rezheight1,
+			onChange: function(h){
+				$('flevel-catalog').setStyles({top: -h});
+			}
+		}).set(0);
+	};
 
 	fheight();
-
-	if(rezheight1 >= 0) {
-		$('cat-scrollbar').setStyles({visibility: 'visible'});
-	}
+	scrollbar();
 	
-	var scroll1 = new Slider($('cat-scroll-line'), $('cat-scroller'), { 
-		mode: 'vertical',
-		steps: rezheight1,
-		onChange: function(h){
-			$('flevel-catalog').setStyles({top: -h});
-		}
-
-	 }).set(0);
+	$('catzag1').addEvent('click', function(){
+		slide2.slideOut();
+		slide3.slideOut();
+		slide4.slideOut();
+		slide5.slideOut();
+		slide1.slideIn();
+		setTimeout("fheight()", 1000);
+		setTimeout("scrollbar()", 1000);
+		setTimeout("catscroller()", 1005);
+	});
+	
+	$('catzag2').addEvent('click', function(){
+		slide1.slideOut();
+		slide3.slideOut();
+		slide4.slideOut();
+		slide5.slideOut();
+		slide2.slideIn();
+		setTimeout("fheight()", 1000);
+		setTimeout("scrollbar()", 1000);
+		setTimeout("catscroller()", 1005);
+	});
+	
+	$('catzag3').addEvent('click', function(){
+		slide2.slideOut();
+		slide1.slideOut();
+		slide4.slideOut();
+		slide5.slideOut();
+		slide3.slideIn();
+		setTimeout("fheight()", 1000);
+		setTimeout("scrollbar()", 1000);
+		setTimeout("catscroller()", 1005);
+	});
+	
+	$('catzag4').addEvent('click', function(){
+		slide2.slideOut();
+		slide3.slideOut();
+		slide1.slideOut();
+		slide5.slideOut();
+		slide4.slideIn();
+		fheight();
+		setTimeout("fheight()", 1000);
+		setTimeout("scrollbar()", 1000);
+		setTimeout("catscroller()", 1005);
+	});
+	
+	$('catzag5').addEvent('click', function(){
+		slide2.slideOut();
+		slide3.slideOut();
+		slide4.slideOut();
+		slide1.slideOut();
+		slide5.slideIn();
+		setTimeout("fheight()", 1000);
+		setTimeout("scrollbar()", 1000);
+		setTimeout("catscroller()", 1005);
+	});
+	
+	$('razvcat').addEvent('click', function(){
+		slide1.slideIn();
+		slide2.slideIn();
+		slide3.slideIn();
+		slide4.slideIn();
+		slide5.slideIn();
+		setTimeout("fheight()", 1000);
+		setTimeout("scrollbar()", 1000);
+		setTimeout("catscroller()", 1005);
+	});
 	 
  	var rezheight2, heightcat2, height2;
 	
 	sheight = function (){
-		heightcat2 = $('flevel-catalog').offsetHeight;
-		height2 = $('cat-list').offsetHeight;
+		heightcat2 = $('slevel-cat').offsetHeight;
+		height2 = $('soder-cat-wrapper').offsetHeight;
 
 		rezheight2 = heightcat2 - height2;	
 		return rezheight2;
@@ -84,5 +169,24 @@ window.addEvent('domready', function(){
 		}
 	});
 	
+	var catpos = 0;
+	var catdvig = new Fx.Morph($('map'));
+
+	$('razvmap').addEvent('click', function(){
+		if(catpos == 0) {
+			$('catalog').setStyles({
+				'display': 'none'
+			});
+			catdvig.start({ 'margin-left': 30 });
+			catpos = 1;
+		} else {
+			catdvig.start({ 'margin-left': 380 }).chain(function(){
+				$('catalog').setStyles({
+					'display': 'block'
+				});
+				catpos = 0;	
+			});
+		}
+	});
 	
 })
