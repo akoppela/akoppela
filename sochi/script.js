2a65de47051525cd6b020492b725a0f7c5001a9b
unknown <akoppela@gmail.com>
2009-04-03 14:51:54 +0700

    add scrollbar

--- sochi/script.js
+++ sochi/script.js
@@ -77,6 +77,7 @@ var mapSlide = new Class({
 		this.mapSlide = new Fx.Morph(this.map, { duration: 500, fps: 200 });
 		this.butSlide = new Fx.Morph(this.el.getParent()), {duration: 500};
 		this.catMorph = new Fx.Morph(this.cat);
+		this.scrollMorph = new Fx.Morph(this.cat.getElement('.vScrollbar'));
 		this.mapPos = true;
 		
 		this.el.addEvent('click', this.slideMap.bind(this));
@@ -93,6 +94,7 @@ var mapSlide = new Class({
 					this.el.set('class', 'map-left');
 				}.bind(this));
 			}.bind(this));
+			this.cat.getElement('.vScrollbar').set('opacity', 0);
 			this.butSlide.start({
 				'margin-left': 0
 			});
@@ -138,6 +140,7 @@ var menuSlide = new Class({
 			this.LevelTwo.slide('hide');
 		} else {
 			this.LevelTwo.slide('show');
+			this.menuHead.getParent('.first-level-cat').set('class', 'first-level-cat active-cat');
 		}
 		this.menuHead.addEvent('click', this.toggleMenu);
 	},
@@ -166,20 +169,174 @@ var menuSlide = new Class({
 
 var Scrollbar = new Class({
 	
-	initialize: function(sbar){
+	Implements: [Options],
+	
+	options: {
+		vDur: 2000
+	},
+	
+	initialize: function(sbar, main, options){
+		this.setOptions(options)
+		
 		this.sbar = $(sbar);
-		if(!this.sbar) return false;
+		this.main = $(main);
+		if(!this.sbar || !this.main) return false;
 		
 		this.arrowUp = this.sbar.getElement('.arrowUp');
 		this.arrowDown = this.sbar.getElement('.arrowDown');
 		this.vThumb = this.sbar.getElement('.vThumb');
 		this.vTrack = this.sbar.getElement('.vTrack');
 		
-		this.vThumb.addEvent('mousedown', this.vDrag.bind(this));
+		this.hsteps = this.main.getParent('.moocontent').getHeight() - this.main.getHeight();
+		this.steps = this.hsteps;
+		this.vTrackLine = this.vTrack.getHeight() - this.vThumb.getHeight();
+		
+		this.fxscroll = new Slider(this.vTrack, this.vThumb, {
+				mode: 'vertical',
+				steps: this.hsteps,
+				onChange: function(h){
+					this.textPause();
+					this.main.setStyles({top: h * this.hsteps / this.steps });
+					return false;
+				}.bind(this)
+		});
+		
+		this.arrowUp.addEvents({
+			'mousedown': this.textUp.bind(this),
+			'mouseup': this.textPause.bind(this)
+		});
+		this.arrowDown.addEvents({
+			'mousedown': this.textDown.bind(this),
+			'mouseup': this.textPause.bind(this)
+		});
+		
+		this.sbar.set('morph', {duration: 500});
+		this.vThumb.set('morph', {duration: this.options.vDur});
+		this.main.set('morph', {duration: this.options.vDur});
+		this.main.getParent('.moocontent').addEvent('mousewheel', this.textWheel.bind(this));
+		this.main.addEvent('mousemove', this.updatePos.bind(this));
+		this.updatePos();
 	},
 	
-	vDrag: function(event){
+	textUp: function(){
+		this.dur('up');
+		this.main.morph({
+			top: 0
+		});
+		this.vThumb.morph({
+			top: 0
+		});
+		return false;
+	},
+	
+	textDown: function(){
+		this.dur('down');
+		this.main.morph({
+			top: this.hsteps
+		});
+		this.vThumb.morph({
+			top: this.vTrack.getHeight() - this.vThumb.getHeight()
+		});
+		return false;
+	},
+	
+	textPause: function(){
+		this.dur('pause');
+		return false;
+	},
+	
+	getPos: function(){
+		this.scrollerpos = this.vThumb.getStyle('top').toInt();
+		this.blockpos = this.main.getStyle('top').toInt()
+		return false;
+	},
 		
+	dur: function(pos){
+		this.getPos();
+		if(pos == 'up'){
+			this.durat = this.scrollerpos * this.options.vDur / this.vTrackLine;
+			if(this.durat == 0) this.durat = this.options.vDur;
+			this.stepScroll = this.scrollerpos - this.vTrackLine / 5;
+			this.stepMain = this.blockpos - this.hsteps / 5;
+			if(this.stepScroll < 0) this.stepScroll = 0;
+			if(this.stepMain > 0) this.stepMain = 0;
+		} else if(pos == 'down'){
+			this.durat = this.options.vDur - this.scrollerpos * this.options.vDur / this.vTrackLine;
+			if(this.durat == 0) this.durat = this.options.vDur;
+			this.stepScroll = this.scrollerpos + this.vTrackLine / 5;
+			this.stepMain = this.blockpos + this.hsteps / 5;
+			if(this.stepScroll > this.vTrackLine) this.stepScroll = this.vTrackLine;
+			if(this.stepMain < this.hsteps) this.stepMain = this.hsteps;
+		} else if(pos == 'pause') {
+			this.durat = 0;
+		}
+		this.vThumb.set('morph', {duration: this.durat});
+		this.main.set('morph', {duration: this.durat});
+		return false;
+	}, 
+	
+	textWheel: function(e){
+		if(this.hsteps < 0){
+			if(e.wheel > 0) {
+				this.dur('up');
+				this.main.morph({
+					top: this.stepMain
+				});
+				this.vThumb.morph({
+					top: this.stepScroll
+				});
+			} else if(e.wheel < 0) {
+				this.dur('down');
+				this.main.morph({
+					top: this.stepMain 
+				});
+				this.vThumb.morph({
+					top: this.stepScroll
+				});
+			}
+		}
+		return false;
+	},
+	
+	scrollOp: function(){
+		if(this.hsteps > 0){
+			this.sbar.morph({
+				opacity: 0
+			});
+		} else {
+			this.sbar.morph({
+				opacity: 1
+			});
+		}
+	},
+	
+	updatePos: function(){
+		this.curHeight = this.main.getParent('.moocontent').getHeight() - this.main.getHeight();
+		
+		if(this.curHeight != this.hsteps) {
+			this.raznHeight = this.curHeight - this.hsteps;
+			this.hsteps = this.curHeight;
+			this.setPos();
+		}
+		this.scrollOp();
+	},
+	
+	setPos: function(){
+		this.getPos();
+		this.dur('up');
+		
+		if(this.curHeight < 0){
+			this.main.morph({
+				top: this.scrollerpos * this.curHeight / this.vTrackLine
+			});
+		} else {
+			this.vThumb.morph({
+				top: 0
+			});
+			this.main.morph({
+				top: 0
+			});
+		}
 	}
 	
 });
@@ -189,7 +346,8 @@ window.addEvent('domready', function(){
 	var searchText = new inputText('search-text');
 	var ms = new mapSlide('razvmap', 'catalog');
 	var catSlide = new menuSlide('flevel-catalog');
-	var catScroller = new Scrollbar('cat-scrollbar');
+	var catScroller = new Scrollbar('cat-scrollbar', 'flevel-catalog');
+	var cat2Scroller = new Scrollbar('cat-tree-scrollbar', 'slevel-cat');
 	
 	$('razvcat').addEvent('click', function(){ catSlide.showAll(); });
 	
