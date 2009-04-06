37e4357c66239309589452eed0a60a357df9162a
unknown <akoppela@gmail.com>
2009-04-06 17:21:57 +0700

    fixed mj ie6 png

--- sochi/script.js
+++ sochi/script.js
@@ -256,15 +256,15 @@ var Scrollbar = new Class({
 		if(pos == 'up'){
 			this.durat = this.scrollerpos * this.options.vDur / this.vTrackLine;
 			if(this.durat == 0) this.durat = this.options.vDur;
-			this.stepScroll = this.scrollerpos - this.vTrackLine / 5;
-			this.stepMain = this.blockpos - this.hsteps / 5;
+			this.stepScroll = this.scrollerpos - this.vTrackLine / 3;
+			this.stepMain = this.blockpos - this.hsteps / 3;
 			if(this.stepScroll < 0) this.stepScroll = 0;
 			if(this.stepMain > 0) this.stepMain = 0;
 		} else if(pos == 'down'){
 			this.durat = this.options.vDur - this.scrollerpos * this.options.vDur / this.vTrackLine;
 			if(this.durat == 0) this.durat = this.options.vDur;
-			this.stepScroll = this.scrollerpos + this.vTrackLine / 5;
-			this.stepMain = this.blockpos + this.hsteps / 5;
+			this.stepScroll = this.scrollerpos + this.vTrackLine / 3;
+			this.stepMain = this.blockpos + this.hsteps / 3;
 			if(this.stepScroll > this.vTrackLine) this.stepScroll = this.vTrackLine;
 			if(this.stepMain < this.hsteps) this.stepMain = this.hsteps;
 		} else if(pos == 'pause') {
